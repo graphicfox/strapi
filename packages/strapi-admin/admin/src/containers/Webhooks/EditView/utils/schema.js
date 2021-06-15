47e247df720555fff921d5772a58c66ae87d52df
Pierre Noël <petersg83@gmail.com>
2021-06-15 17:07:11 +0200

    upgrade yup to 0.32.9

--- packages/strapi-admin/admin/src/containers/Webhooks/EditView/utils/schema.js
+++ packages/strapi-admin/admin/src/containers/Webhooks/EditView/utils/schema.js
@@ -29,7 +29,10 @@ const schema = yup.object().shape({
       })
     );
   }),
-  events: yup.array().required(translatedErrors.required),
+  events: yup
+    .array()
+    .min(1)
+    .required(translatedErrors.required),
 });
 
 export default schema;
